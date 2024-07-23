82b5e234c3e6dd0d0a1012fb599743a38f2caaa0
denis.zubrin <dendel1998@gmail.com>
2024-07-23 14:43:13 +0300

    Codebase

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { 
+    enabled: false,
+  },
+  alias: {
+    '@': '/src',
+  },
+  css: ['@/shared/assets/styles/style.scss'],
+  dir: {
+    pages: './src/app/routes',
+    layouts: './src/app/layouts',
+  },
+  modules: ['@nuxt/eslint'],
+  // router: {
+  //   base: '/vue3_nuxt_adaptive/'
+  // },
+  // app: {
+  //   baseURL: '/vue3_nuxt_adaptive/'
+  // },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      failOnError: false, 
+    },
+  },
+});
---
+++ src/app/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <Header />
+    <main class="main">
+      <!-- output the page content-->
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
+<script setup>
+import Header from '@/widgets/Header';
+import Footer from '@/widgets/Footer';
+
+</script>
+<style lang="scss"></style>
